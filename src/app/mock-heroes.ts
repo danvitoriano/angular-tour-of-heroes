249a6615a83ddfec042cc80e111a1727f43fb979
Danilo Vitoriano <vitoriano08@gmail.com>
2020-02-15 05:19:41 -0300

    exibe lista e detalhes de um heroi˜

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+  { id: 9, name: "Spider-Man" },
+  { id: 10, name: "Captain Marvel" },
+  { id: 11, name: "Hulk" },
+  { id: 12, name: "Moon Girl" },
+  { id: 13, name: "Black Panther" },
+  { id: 14, name: "Thanos" },
+  { id: 15, name: "Iron Man" },
+  { id: 17, name: "Black Widow" },
+  { id: 18, name: "Wolverine" },
+  { id: 19, name: "Invisible Woman" }
+];
